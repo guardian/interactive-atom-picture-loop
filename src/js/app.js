e2eeb5034ca9405a363b5b37537cf5a81112b23c
Duarte Carrilho da Graça <duarte.carrilho.casual@theguardian.com>
2018-11-15 16:47:52 +0000

    everythign except the video

--- src/js/app.js
+++ src/js/app.js
@@ -1,91 +1,85 @@
 // javascript goes here
-
+var d;
 import loadJson from '../components/load-json/'
 
-console.log('2');
+var pictureLoops = getPictureLoops();
+processIfWinner(pictureLoops);
 
-loadJson("https://interactive.guim.co.uk/docsdata-test/1BKSKfM9hmF6H-q_YP38_KxMcuHLopKFPBj0n7iQKEbw.json")
-  .then((rawSheet) => {
-    
+function getPictureLoops() {
+  var parentDoc = window.parent.document;
 
-    var data = rawSheet.sheets.Sheet1;
-    
-    var embeds = document.querySelectorAll('.interactive-audio-player-wrapper');
-    
-    for (var i = 0; i < embeds.length; ++i) {
-      var embed = embeds[i];
-      if (embed && data[i] && !embed.classList.contains('processed')) {
-        embed.classList.add('processed');
-        embed.querySelector('.caption-wrapper__text').innerText = data[i]['Caption'];
-        
-        var audio = new Audio(data[i]['MP3']);
-        audio.setAttribute('preload', 'metadata');
-        embed.appendChild(audio);
-
-        setupAudio(embed);
+  var allAtoms = [].slice.call(parentDoc.querySelectorAll('.element-atom'));
+  return allAtoms.filter(function(atom) {
+    if (atom.hasAttribute('data-alt') == true) {
+      var atomAlt = atom.getAttribute('data-alt').trim();
+      if (atomAlt.indexOf('https://interactive.guim.co.uk/docsdata')==0) {
+        return true;
       }
     }
   });
-
-
-function setupAudio(embed) {
-  var wrapper = embed.querySelector('.player');
-  var playButton = wrapper.querySelector('.player__play-pause');
-  var audio = embed.querySelector('audio');
-  var ellipseFull = embed.querySelector('.circle.full ellipse');
-
-  audio.addEventListener('loadedmetadata', function() {
-    var formattedDuration = fmtMSS(audio.duration);
-    wrapper.querySelector('.player__play-pause').dataset.duration = formattedDuration;
-  });
-  
-  audio.addEventListener('timeupdate', function() {
-    var playRatio = audio.currentTime/audio.duration;
-    var playStroke = Math.round(playRatio*100);
-    if (playRatio) {
-      ellipseFull.setAttribute('stroke-dasharray', '0 '+playStroke+' 100');
-    }
-  });
-
-  // Play events
-  audio.addEventListener('playing', function() { wrapper.dataset.paused = 'false'; });
-  audio.addEventListener('play', function() { wrapper.dataset.paused = 'false'; });
-
-  // Pause events
-  audio.addEventListener('pause', function() { wrapper.dataset.paused = 'true'; });
-  audio.addEventListener('stalled', function() { wrapper.dataset.paused = 'true'; });
-  audio.addEventListener('waiting', function() { wrapper.dataset.paused = 'true'; });
-  audio.addEventListener('ended', function() {
-    wrapper.dataset.paused = 'true';
-    ellipseFull.setAttribute('stroke-dasharray', '0 0 100');
-  });
-
-  playButton.addEventListener('click', function() {
-    if (audio.paused) {
-      pauseAllAudio();
-      audio.play();
-      wrapper.dataset.paused = 'false';
-    } else {
-      audio.pause();
-      wrapper.dataset.paused = 'true';
-    }
-  });
 }
 
-function fmtMSS(sf){
-  var s = Math.round(sf);
-  return(s-(s%=60))/60+(9<s?':':':0')+s;
-}
+function processIfWinner(pictureLoops) {
+  var keyAtom = pictureLoops[0];
+  if (!keyAtom.classList.contains('processed')) {
+    console.log('Processing based on', keyAtom);
+    
+    // Process all the atoms here
+    keyAtom.classList.add('processed');
 
+    var jsonUrl = keyAtom.getAttribute('data-alt').trim();
+    
+    loadJson(jsonUrl).then((rawSheet) => {
+      
+      // written this way so they can change the name of the tab and it won't break
+      var firstTabName = Object.keys(rawSheet.sheets)[0]
+      var data = rawSheet.sheets[firstTabName];
+      
+      pictureLoops.forEach(function(atom, i) {
+        var atomDoc = atom.querySelector('iframe').contentWindow.document;
+        if (data[i]) {
+          makePictureLoop(atomDoc, atom, data[i]);
+        } else {
+          
+          console.warn('Insufficient data in the google sheet to create ', i+1, ' picure loops . Removing element between',  atom.previousElementSibling, 'and', atom.nextElementSibling);
+          atom.remove();
+          
+        }
+      })
+    });
 
-function pauseAllAudio() {
-  var allAudio = document.querySelectorAll('audio');
-  for (var i = 0; i < allAudio.length; ++i) {
-    var audio = allAudio[i];
-    var wrapper = audio.closest('.interactive-audio-player-wrapper').querySelector('.player');
-    if (wrapper && !audio.paused) {
-      audio.pause();
-      wrapper.dataset.paused = 'true';
-    }
   }
 }
+
+
+function makePictureLoop(atom, atomWrapper, data) {
+  var videoEl = atom.querySelector('.picture-loop__video');
+  var imgEl = atom.querySelector('.picture-loop__image');
+  
+  imgEl.setAttribute('src', data.photo);
+  imgEl.addEventListener('load', function() {
+    window.resize();
+  })
+
+}
+
+
+
+
+// function fmtMSS(sf){
+//   var s = Math.round(sf);
+//   return(s-(s%=60))/60+(9<s?':':':0')+s;
+// }
+
+
+// function pauseAllAudio() {
+//   var allAudio = document.querySelectorAll('audio');
+//   for (var i = 0; i < allAudio.length; ++i) {
+//     var audio = allAudio[i];
+//     var wrapper = audio.closest('.interactive-audio-player-wrapper').querySelector('.player');
+//     if (wrapper && !audio.paused) {
+//       audio.pause();
+//       wrapper.dataset.paused = 'true';
+//     }
+//   }
+// }
